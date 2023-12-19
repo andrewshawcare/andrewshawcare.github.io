5f247ed9ca9d72837808a1254112c6da19656431
Andrew Shaw Care <andrewshawcare@gmail.com>
2023-12-19 09:41:07 -0500

    Use Markdoc file

--- new/index.js
+++ new/index.js
@@ -3,13 +3,14 @@ import path from "node:path";
 import url from "node:url";
 import Markdoc from "@markdoc/markdoc";
 
-const content = "# Hello, world?";
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const content = fs.readFileSync(path.join(__dirname, "index.md")).toString("utf8");
 const node = Markdoc.parse(content);
 const transformedNode = Markdoc.transform(node);
 const html = Markdoc.renderers.html(transformedNode);
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const buildPath = path.join(__dirname, "build")
 
 fs.mkdirSync(buildPath, { recursive: true });
